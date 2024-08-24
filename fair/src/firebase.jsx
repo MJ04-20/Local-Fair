37707e71912cef3a672911703b386f9e2c6d9fa2
Kripa Shankar <kshankar.1109@gmail.com>
2024-08-25 02:22:01 +0530

    hosted

---
+++ fair/src/firebase.jsx
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA277zgd2Ztjf0l0Zx9sKt3XuePUbH79Vw",
+  authDomain: "price-9d9bd.firebaseapp.com",
+  projectId: "price-9d9bd",
+  storageBucket: "price-9d9bd.appspot.com",
+  messagingSenderId: "620496495713",
+  appId: "1:620496495713:web:b7f28e99659c803b017620"
+};
+
+const app = initializeApp(firebaseConfig);
